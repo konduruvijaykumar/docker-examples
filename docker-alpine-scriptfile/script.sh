fdd867411f61a2ab26528c781981cd922954254d
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-10-26 23:50:44 +0530

    Vijay - 2017/10/26 - Docker examples

---
+++ docker-alpine-scriptfile/script.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+# https://forums.docker.com/t/getting-panic-spanic-standard-init-linux-go-178-exec-user-process-caused-no-such-file-or-directory-red-while-running-the-docker-image/27318/4
+
+echo hello world, from a script file!
