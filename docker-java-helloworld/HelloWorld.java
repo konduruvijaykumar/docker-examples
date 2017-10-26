d619c45afac50c5aa2aff167f0565a57e9f429b6
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-10-26 23:58:23 +0530

    Vijay - 2017/10/26 - Docker examples

---
+++ docker-java-helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * @author Vijay
+ *
+ */
+// https://github.com/stevenalexander/docker-java8-helloworld
+// https://github.com/prabin5/docker
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello! Welcome to Docker World - Version 1");
+	}
+
+}
